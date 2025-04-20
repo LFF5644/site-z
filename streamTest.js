08f5134c6e6f87521c352601c13277c5c2cbdec9
LFF5644 (LFF-Vivobook) <meerschweinchen5644@gmail.com>
2025-04-21 01:23:24 +0200

    another test

---
+++ streamTest.js
@@ -0,0 +1,18 @@
+<?
+const child_process=require("child_process");
+
+
+response.setHeader("Content-Type", "video/mp4");
+response.setHeader("Transfer-Encoding","chunked");
+
+const ffmpeg=child_process.spawn("/usr/bin/ffmpeg",[
+	'-i', '/dev/video0',
+	'-vb', '100k',
+	'-preset', 'ultrafast',
+	'-acodec', 'copy',
+	'-f', 'mjpeg',
+	'pipe:1',
+]);
+ffmpeg.stdout.pipe(response);
+
+?>
